77e1af479f53999ce50b7cf0367bdb09444efaca
Matt Ficken <v-mafick@microsoft.com>
2013-05-16 00:33:43 -0700

    check PHP_SDK environment variable
    Former-commit-id: ea4fe6d07709fa1644d2057144be7607bb576507

---
+++ src/com/mostc/pftt/scenario/NginxScenario.java
@@ -0,0 +1,50 @@
+package com.mostc.pftt.scenario;
+
+import com.mostc.pftt.host.AHost;
+import com.mostc.pftt.model.core.ESAPIType;
+import com.mostc.pftt.model.sapi.WebServerManager;
+
+/** NOT_IMPLEMENTED
+ * 
+ * Second most popular web server (after Apache).
+ * 
+ * Use FastCGI to run PHP with nginx.
+ * 
+ * @see http://nginx.org/en/docs/windows.html
+ *
+ */
+
+public class NginxScenario extends AbstractWebServerScenario {
+	
+	public NginxScenario() {
+		this(null);
+	}
+
+	protected NginxScenario(WebServerManager smgr) {
+		super(smgr);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getTestThreadCount(AHost host) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ESAPIType getSAPIType() {
+		return ESAPIType.FAST_CGI;
+	}
+
+	@Override
+	public String getName() {
+		return "Nginx";
+	}
+
+	@Override
+	public boolean isImplemented() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
