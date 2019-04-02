d3b6b43a898e4c6628df60e968a73cbbd0189900
Allen Truong <v-altruo@microsoft.com>
2019-04-02 13:11:13 -0700

    Issue #11: Created .bat file to package appropriate class files for QA host
    Issue 11: Updated it to put pftt2.jar into the package directly.
    
    Issue #11: Minor change
    
    Issue #11: Renamed files, updated to use variables
    
    Issue #11: Updated for when batch file finishes, user will remain in starting directory.

---
+++ bin/pftt2_release.bat
@@ -0,0 +1,37 @@
+@ECHO OFF
+
+REM set important env vars
+IF DEFINED PFTT_SHELL GOTO :skip_set_env
+CALL %~dp0set_env.cmd
+:skip_set_env
+
+SET PFTT_LIB=%PFTT_HOME%\lib
+SET PFTT_BUILD=%PFTT_HOME%\build
+
+REM Check if build directory exists
+if exist %PFTT_BUILD% (
+	REM Create pftt_release directory in main folder
+	md %PFTT_HOME%\pftt_release
+	SET PFTT_RELEASE=%PFTT_HOME%\pftt_release
+
+	REM Copy contents of bin, conf and lib to respective folders
+	md %PFTT_RELEASE%\bin
+	md %PFTT_RELEASE%\conf
+	md %PFTT_RELEASE%\lib
+
+	xcopy /s /i "%PFTT_BIN%" "%PFTT_RELEASE%\bin"
+	xcopy /s /i "%PFTT_CONF%" "%PFTT_RELEASE%\conf"
+	xcopy /s /i "%PFTT_LIB%" "%PFTT_RELEASE%\lib"
+
+	REM Create pftt2.jar in lib of the package
+	jar cf pftt2.jar -C %PFTT_BUILD% com %PFTT_BUILD%\org\columba %PFTT_BUILD%\org\kxml2 %PFTT_BUILD%\org\incava
+	MOVE pftt2.jar %PFTT_RELEASE%\lib
+
+	REM Create zip file of pftt_release folder
+	%PFTT_BIN%\7za.exe a -tzip %PFTT_HOME%\pftt_release.zip %PFTT_HOME%\pftt_release
+
+	REM Delete temp files/folders
+	rd /s /q %PFTT_HOME%\pftt_release
+) else (
+	ECHO Build folder does not exist
+)
