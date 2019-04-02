4586967cba85411660ec55d97702bf0dedd590e1
Allen Truong <v-altruo@microsoft.com>
2019-04-02 13:11:13 -0700

    Issue #11: Created .bat file to package appropriate class files for QA host

---
+++ bin/pftt2-release.bat
@@ -0,0 +1,34 @@
+@ECHO OFF
+
+REM Check if build directory exists
+cd ../
+if exist build (
+	REM Create pftt-release directory in main folder
+	mkdir pftt-release
+
+	REM Create pftt2.jar and place in lib folder
+	cd build
+	jar cf pftt2.jar ./com/ ./org/columba ./org/kxml2 ./org/incava
+	MOVE ./pftt2.jar ../lib
+	cd ..
+
+	REM Copy contents of bin, conf and lib to respective folders
+	cd /d pftt-release
+	mkdir bin
+	mkdir conf
+	mkdir lib
+	cd ..
+
+	xcopy /s /i "./bin" "./pftt-release/bin"
+	xcopy /s /i "./conf" "./pftt-release/conf"
+	xcopy /s /i "./lib" "./pftt-release/lib"
+
+	REM Create zip file of pftt-release folder
+	"./bin/7za.exe" a -tzip "./pftt-release.zip" "./pftt-release"
+
+	REM Delete temp files/folders
+	rd /s /q pftt-release
+	cd ./bin
+) else (
+	ECHO Build folder does not exist
+)
