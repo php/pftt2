505ccadd4290b817a22d5f2a21be0bb68e5223e4
Allen Truong <v-altruo@microsoft.com>
2019-04-02 14:31:26 -0700

    Issue 11: Updated it to put pftt2.jar into the package directly.

--- bin/pftt2-release.bat
+++ bin/pftt2-release.bat
@@ -6,12 +6,6 @@ if exist build (
 	REM Create pftt-release directory in main folder
 	mkdir pftt-release
 
-	REM Create pftt2.jar and place in lib folder
-	cd build
-	jar cf pftt2.jar ./com/ ./org/columba ./org/kxml2 ./org/incava
-	MOVE ./pftt2.jar ../lib
-	cd ..
-
 	REM Copy contents of bin, conf and lib to respective folders
 	cd /d pftt-release
 	mkdir bin
@@ -22,6 +16,12 @@ if exist build (
 	xcopy /s /i "./bin" "./pftt-release/bin"
 	xcopy /s /i "./conf" "./pftt-release/conf"
 	xcopy /s /i "./lib" "./pftt-release/lib"
+
+	REM Create pftt2.jar and place in lib of the package
+	cd build
+	jar cf pftt2.jar ./com/ ./org/columba ./org/kxml2 ./org/incava
+	MOVE ./pftt2.jar ../pftt-release/lib
+	cd ..
 
 	REM Create zip file of pftt-release folder
 	"./bin/7za.exe" a -tzip "./pftt-release.zip" "./pftt-release"
