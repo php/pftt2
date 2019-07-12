7c40ee87a37e52bcfe479bf2bc2261db4869d08d
Allen Truong <v-altruo@microsoft.com>
2019-07-12 21:18:42 +0200

    Fixes #53: Created batch files

---
+++ bin/get_latest_revision.cmd
@@ -0,0 +1,10 @@
+set SNAP_JSON=php-%branch%.json
+set SNAP_JSON_URL=https://windows.php.net/downloads/snaps/php-%branch%/%SNAP_JSON%
+bitsadmin /transfer DownloadingJson /download /priority high !SNAP_JSON_URL! %PFTT_CACHE%\!SNAP_JSON!
+
+set "psCmd="add-type -As System.Web.Extensions;^
+$JSON = new-object Web.Script.Serialization.JavaScriptSerializer;^
+$JSON.DeserializeObject($input).revision_last_exported""
+
+for /f %%I in ('^<"%PFTT_CACHE%\!SNAP_JSON!" powershell -noprofile %psCmd%') do set "revision=r%%I"
+del %PFTT_CACHE%\!SNAP_JSON!
