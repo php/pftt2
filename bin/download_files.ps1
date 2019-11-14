fea34403b052f966fe795268d1e8d28b27c50499
Dale Hirt <dalehirt@microsoft.com>
2019-11-14 10:39:53 -0800

    Make download_files.ps1 more robust.

--- bin/download_files.ps1
+++ bin/download_files.ps1
@@ -1,11 +1,25 @@
-Param([string]$url, [string]$path, [string]$file_name)
+[cmdletbinding(SupportsShouldProcess=$true)]
+Param(
+	[Parameter(Mandatory)]
+	[string]$url, 
+	
+	[Parameter(Mandatory)]
+	[string]$path, 
+	
+	[Parameter(Mandatory)]
+	[string]$file_name
+)
 
 If ($url -eq "" -or $path -eq "" -or $file_name -eq "") {
 	Write-Output "User error: Must specifiy url, save path and file name"
 	Write-Output "download_file <url> <path to dir> <desired file name>"
 }
 
-Import-Module BitsTransfer
-
-Write-Host "Downloading" $file_name"..." -ForegroundColor Green
-	Start-BitsTransfer -Source $url -Destination $path
+$fullPath = join-path $path $file_name
+Write-Host "Downloading $($url) to $($file_name)..." -ForegroundColor Green
+if ($PSCmdlet.ShouldProcess($fullPath, "Download $url")) {
+	[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]'Tls,Tls11,Tls12'	
+	$wc = New-Object System.Net.WebClient
+	$wc.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)")
+	$wc.DownloadFile($url, $fullPath)
+}
